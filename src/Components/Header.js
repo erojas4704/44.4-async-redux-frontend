9881b57aba7eb642a658068d37eca1e12bb13278
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-10-09 10:32:48 -0400

    Cleaned up nasty language

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,11 @@ export default function Header() {
     return (
         <div className="jumbotron">
             <h1 className="display-4">Microblog</h1>
-            <p className="lead">Spew your bullshit here on the internet nobody cares.</p>
+            <p className="lead">
+                Tell the internet your opinion.
+                Please.
+                The opinions of absolute nobodies are in high demand.
+            </p>
             <nav className="blog-nav">
                 <Link to="/">Blog</Link>
                 <Link to="/new">Add a new post</Link>
